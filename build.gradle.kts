a8b19348987846f36f20982abcff074b18bf6eb2
= <=>
2024-03-04 11:25:22 +0300

    add basic bd table (dished) and empty controler

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,33 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
-group = "org.example"
+group = "ru.hse._223"
 version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+//    implementation(project(":api"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.liquibase:liquibase-core")
+    implementation("org.postgresql:postgresql")
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
---
+++ src/main/java/ru/hse/_223/restaurant/controllers/DishController.java
@@ -0,0 +1,9 @@
+package ru.hse._223.restaurant.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/dishes")
+public class DishController {
+}
